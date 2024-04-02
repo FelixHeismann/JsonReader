41379d64e0287500c86216326b105caee58f2cc1
highxtrtys <highxtrtys@gmail.com>
2024-04-02 20:03:34 +0200

    gui 4

--- src/GameGui.java
+++ src/GameGui.java
@@ -4,31 +4,35 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class GameGui {
-
+    private JButton a;
+    private JButton b;
+    private JButton c;
+    private JButton d;
+    private JLabel frage;
         public GameGui(Frage f){
             JFrame frame = new JFrame();
             frame.setSize(1920, 1080);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setLayout(null);
-            JButton a = addButtons(f.getAntworten().getA(),200, 200);
-            JButton b = addButtons(f.getAntworten().getB(),600, 200);
-            JButton c = addButtons(f.getAntworten().getC(),200, 600);
-            JButton d = addButtons(f.getAntworten().getD(),600, 600);
+            a = addButtons(f.getAntworten().getA(),200, 200);
+            b = addButtons(f.getAntworten().getB(),600, 200);
+            c = addButtons(f.getAntworten().getC(),200, 600);
+            d = addButtons(f.getAntworten().getD(),600, 600);
             frame.add(a);
             frame.add(b);
             frame.add(c);
             frame.add(d);
             frame.setBackground(Color.yellow);
-            a.addActionListener(e -> selectionButtonPressed(a));
-            b.addActionListener(e -> selectionButtonPressed(b));
-            c.addActionListener(e -> selectionButtonPressed(c));
-            d.addActionListener(e -> selectionButtonPressed(d));
             frame.setVisible(true);    
-            JLabel frage = new JLabel();
+            frage = new JLabel();
             frage.setText(f.getFrage());
             frage.setSize(400, 100);
             frage.setBackground(Color.cyan);
             frame.add(frage);
+            a.addActionListener(e -> selectionButtonPressed(f, a));
+            b.addActionListener(e -> selectionButtonPressed(f, b));
+            c.addActionListener(e -> selectionButtonPressed(f, c));
+            d.addActionListener(e -> selectionButtonPressed(f, b));
         }
 
         public JButton addButtons(String text, int xc, int yc){
@@ -39,7 +43,17 @@ public class GameGui {
             return button;
         }
 
-        public String selectionButtonPressed(JButton button){
-            return button.getText();
+        public void selectionButtonPressed(Frage f, JButton j){
+            if(f.getRichtige_antwort()==j.getText()){
+                main.nextQuestions();   
+            }
+        }
+
+        public void changeButtons(Frage f){
+            a.setText(f.getAntworten().getA());
+            b.setText(f.getAntworten().getB());
+            c.setText(f.getAntworten().getC());
+            d.setText(f.getAntworten().getD());
+            frage.setText(f.getFrage());
         }
 }
