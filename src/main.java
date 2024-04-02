c99e0d331a7eea091f0d9c1f0d7b9dbd9a149b33
highxtrtys <highxtrtys@gmail.com>
2024-04-02 19:46:01 +0200

    gui 4

--- src/main.java
+++ src/main.java
@@ -1,4 +1,4 @@
-public class main {
+public class main { 
     public static void main(String[] args){
         Reader reader = new Reader();
         GameGui gui = new GameGui(reader.getRandomFrage());
