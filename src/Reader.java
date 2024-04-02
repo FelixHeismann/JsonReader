1a58a4d91ed9bf57df936e5fdbc252a55013f39b
Felix Heismann <felix.heismann@gmail.com>
2024-04-02 19:27:12 +0200

    k

--- src/Reader.java
+++ src/Reader.java
@@ -21,6 +21,10 @@ public class Reader {
             System.out.println(ex.toString());
         }
     }
+
+    public static void main(String[] args){
+        
+    }
     
     public Frage getRandomFrage(){
         Random ran = new Random();
