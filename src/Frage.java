cfe6fd4c74dc9bd37ba5388973b07abfd1231f1f
Felix Heismann <felix.heismann@gmail.com>
2024-04-02 12:54:40 +0200

    k

---
+++ src/Frage.java
@@ -0,0 +1,45 @@
+
+public class Frage {
+    int id;
+    String frage;
+    Antworten antworten;
+    String richtige_antwort;
+    int schwierigkeit;
+
+    public Frage(){}
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFrage() {
+        return frage;
+    }
+    public void setFrage(String frage) {
+        this.frage = frage;
+    }
+    public String getRichtige_antwort() {
+        return richtige_antwort;
+    }
+    public void setRichtige_antwort(String richtige_antwort) {
+        this.richtige_antwort = richtige_antwort;
+    }
+    public int getSchwierigkeit() {
+        return schwierigkeit;
+    }
+    public void setSchwierigkeit(int schwierigkeit) {
+        this.schwierigkeit = schwierigkeit;
+    }
+    public Antworten getAntworten() {
+        return antworten;
+    }
+    public void setAntworten(Antworten antworten) {
+        this.antworten = antworten;
+    }
+    
+    
+    
+
+}
---
+++ src/Reader.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class Reader {
+
+    public static List<Frage> ListeFragen;
+
+    public static void main(String[] args) throws Exception {
+
+        File file = new File("C:\\Users\\felix\\Desktop\\Programmieren\\Java Projekte\\Json File Reader\\JsonReader\\lib\\Fragen.json");
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            ListeFragen = mapper.readValue(file, new TypeReference<List<Frage>>(){});
+            for(Frage frage : ListeFragen){
+                System.out.println(frage.frage);
+            }
+        } catch (JsonProcessingException ex) {
+            System.out.println(ex.toString());
+            return;
+        }
+    }
+
+
+}
+
