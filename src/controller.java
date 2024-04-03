c7b15c8f92d7f9e2ad22ae7aa34f985ae061a687
highxtrtys <highxtrtys@gmail.com>
2024-04-03 12:08:45 +0200

    con

--- src/controller.java
+++ src/controller.java
@@ -1,12 +1,15 @@
 public class controller {
+    private int counter;
     private GameGui gui;
     private Reader reader; 
     public controller(){
         reader = new Reader();
+        counter = 1;
         gui = new GameGui(reader.getRandomFrage(), this);
     }
 
     public void nextQuestion(){
+        counter++;
         gui.changeButtons(reader.getRandomFrage());
     }
 }
