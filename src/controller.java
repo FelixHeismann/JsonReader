227d109aee19efea34b169f83d1cb87f270d2a20
Felix Heismann <felix.heismann@gmail.com>
2024-04-09 12:42:11 +0200

    k

--- src/controller.java
+++ src/controller.java
@@ -20,7 +20,7 @@ public class Controller {
             frage = reader.getRandomFrage();
         } 
         Fragen.add(frage);
-        gui.changeButtons(reader.getRandomFrage());
+        gui.changeButtons(frage);
     }
 
     public void restart(){
